b79715f71200e6e5f4af1edaea96a2970ef1c7af
Patrick <skovskiver@gmail.com>
2021-08-30 15:42:17 +0200

    first

---
+++ src/main/java/kea/demo_varekatalog/DemoVarekatalogApplication.java
@@ -0,0 +1,13 @@
+package kea.demo_varekatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVarekatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoVarekatalogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kea/demo_varekatalog/controller/MyController.java
@@ -0,0 +1,7 @@
+package kea.demo_varekatalog.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+}
