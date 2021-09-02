6ffe14ce31b324554e1f78d10070792fef968c54
Patrick <skovskiver@gmail.com>
2021-09-02 11:45:33 +0200

    Index html, pom.xml og en klasse til start af tests added

---
+++ src/test/java/kea/demo_varekatalog/DemoVarekatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.demo_varekatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVarekatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
