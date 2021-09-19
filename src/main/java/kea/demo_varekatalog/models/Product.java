08bca64632588bd433d614ef7545ecfe01acb2b5
Patrick <skovskiver@gmail.com>
2021-09-19 06:43:10 +0200

    Fikset lidt med html og css, for at product page ser pænere ud. Mangler de andre sider og evt lidt på product page

--- src/main/java/kea/demo_varekatalog/models/Product.java
+++ src/main/java/kea/demo_varekatalog/models/Product.java
@@ -32,6 +32,6 @@ public class Product {
 
     @Override
     public String toString(){
-        return "Product name: " + name + " | Product price: " + price;
+        return "Product name: " + name + " <br> Product price: " + price;
     }
 }
